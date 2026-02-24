3a612fbe8d0b15eb1482ef51844da8a038743fef
SuhaibSuliman580 <sohaibsoliman@hotmail.com>
2026-02-25 02:35:50 +0300

    {QCC}

---
+++ bi_branch_all/__manifest__.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'All in One Multiple Branch(Unit) Operation Setup',
+    'version': '17.0.0.0',
+    'category': 'Sales',
+    'author': 'BrowseInfo',
+    'summary': 'All in one Multiple Branch Unit Operation on POS branch Project branch Purchase Requisition Tendor branch Manufacturing branch CRM Multiple Branch Management Multi Branch app Multiple Unit Operating for single company with Multi Branches multi company',
+    "description": """
+
+      branch all in one
+      all in one branch management
+      branch all setup
+      branch all in one setup
+         branch helpdesk and support
+       branch support and helpdesk
+       helpdesk branch
+       helpdesk unit
+       helpdek multiple unit
+       helpdesk operation unit
+       branch crm
+       crm branch
+       crm operating unit
+       crm unit operation management
+       crm multiple unit
+       operating unit crm
+       branch Subscription
+       branch contract
+       Subscription branch management
+       contract branch management
+       operating unit Subscription
+       operating unit contract
+       Subscription unit management
+       contract unit management
+       Subscription operating unit management
+       contract operating unit management
+       branch mrp
+       mrp branch
+       mrp operating unit
+       mrp unit operation management
+      mrp multiple unit
+       operating unit mrp
+       branch manufacturing order
+       manufacturing branch
+       manufacturing operating unit
+       manufacturing unit operation management
+      manufacturing multiple unit
+       operating unit manufacturing
+       multi branch management
+       multi branch application
+       multi operation unit application multi branch odoo multi branch
+       all in one multi branch application multi branch unit operation multi unit operation branch management
+       odoo multi branches management application multi operation mangement
+Multiple Unit operation management for single company, Mutiple Branch management for single company, multiple operation for single company. branching company in odoo , mutliple store ,mutliple company in odoo ,
+    Branch for POS, Branch for Sales, Branch for Purchase, Branch for all, Branch for Accounting, Branch for invoicing, Branch for Payment order, Branch for point of sales, Branch for voucher, Branch for All Accounting reports, Branch Accounting filter.Branch for warehouse, branch for sale stock, branch for location
+  Unit for POS, Unit for Sales, Unit for Purchase, Unit for all, Unit for Accounting, Unit for invoicing, Unit for Payment order, Unit for point of sales, Unit for voucher, Unit for All Accounting reports, Unit Accounting filter.branch unit for warehouse, branch unit for sale stock, branch unit for location
+  Unit Operation for POS, Unit Operation for Sales, Unit operation for Purchase, Unit operation for all, Unit operation for Accounting, Unit Operation for invoicing, Unit operation for Payment order, Unit operation for point of sales, Unit operation for voucher, Unit operation for All Accounting reports, Unit operation Accounting filter.
+  Branch Operation for POS, Branch Operation for Sales, Branch operation for Purchase, Branch operation for all, Branch operation for Accounting, Branch Operation for invoicing, Branch operation for Payment order, Branch operation for point of sales, Branch operation for voucher, Branch operation for All Accounting reports, Branch operation Accounting filter.
+       branch helpdesk and support
+       branch support and helpdesk
+       helpdesk branch
+       helpdesk unit
+       helpdek multiple unit
+       helpdesk operation unit
+       branch crm
+       crm branch
+       crm operating unit
+       crm unit operation management
+       crm multiple unit
+       operating unit crm
+       branch Subscription
+       branch contract
+       Subscription branch management
+       contract branch management
+       operating unit Subscription
+       operating unit contract
+       Subscription unit management
+       contract unit management
+       Subscription operating unit management
+       contract operating unit management
+
+       operating unit for company.
+       multi branch management
+       multi branch application
+       multi operation unit application multi branch odoo multi branch
+       all in one multi branch application multi branch unit operation multi unit operation branch management
+       odoo multi branches management application multi operation mangement
+
+operating Unit for POS,operating Unit for Sales,operating Unit for Purchase,operating Unit for all,operating Unit for Accounting,operating Unit for invoicing,operating Unit for Payment order,operating Unit for point of sales,operating Unit for voucher,operating Unit for All Accounting reports,operating Unit Accounting filter. Operating unit for picking, operating unit for warehouse, operaing unit for sale stock, operating unit for location
+operating-Unit Operation for POS,operating-Unit Operation for Sales,operating-Unit operation for Purchase,operating-Unit operation for all, operating-Unit operation for Accounting,operating-Unit Operation for invoicing,operating-Unit operation for Payment order,operating-Unit operation for point of sales,operating-Unit operation for voucher,operating-Unit operation for All Accounting reports,operating-Unit operation Accounting filter.
+
+       Multiple Unit operation management for single company, Mutiple Branch management for single company, multiple operation for single company.
+    Branch for POS, Branch for Sales, Branch for Purchase, Branch for all, Branch for Accounting, Branch for invoicing, Branch for Payment order, Branch for point of sales, Branch for voucher, Branch for All Accounting reports, Branch Accounting filter.
+  Unit for POS, Unit for Sales, Unit for Purchase, Unit for all, Unit for Accounting, Unit for invoicing, Unit for Payment order, Unit for point of sales, Unit for voucher, Unit for All Accounting reports, Unit Accounting filter.
+  Unit Operation for POS, Unit Operation for Sales, Unit operation for Purchase, Unit operation for all, Unit operation for Accounting, Unit Operation for invoicing, Unit operation for Payment order, Unit operation for point of sales, Unit operation for voucher, Unit operation for All Accounting reports, Unit operation Accounting filter.
+  Branch Operation for POS, Branch Operation for Sales, Branch operation for Purchase, Branch operation for all, Branch operation for Accounting, Branch Operation for invoicing, Branch operation for Payment order, Branch operation for point of sales, Branch operation for voucher, Branch operation for All Accounting reports, Branch operation Accounting filter.
+    """,
+    'website': 'https://www.browseinfo.com',
+    "price": 39.00,
+    'license': 'OPL-1',
+    "currency": 'EUR',
+    'depends': [
+                'base',
+                'branch', 
+                'bi_branch_pos', 
+                'bi_odoo_multi_branch_project',
+                'bi_odoo_multi_branch_tendor',
+                'bi_odoo_mrp_multi_branch',
+                'bi_odoo_crm_multi_branch', 
+                'bi_branch_scrap_order',
+                'bi_multi_branch_pricelist'
+                ],
+
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'live_test_url': 'https://youtu.be/mKA7SsbExus',
+    "images": ['static/description/Banner.gif'],
+
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
